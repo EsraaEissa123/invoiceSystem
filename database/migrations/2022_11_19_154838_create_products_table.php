cf6c7e50bf9fa779b6c2de235a83e5a52140cefe
esraa.eissa123 <esraa.eissa123@gmail>
2023-08-29 06:28:39 +0300

    migration

--- database/migrations/2022_11_19_154838_create_products_table.php
+++ database/migrations/2022_11_19_154838_create_products_table.php
@@ -16,12 +16,12 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->float('sell_price');
-            $table->float('purchase_price');
-            $table->float('box_price');
-            $table->integer('amount')->unsigned()->default(0);    
-            $table->integer('shrink_pieces')->unsigned()->default(0);    
-            $table->integer('box_pieces')->unsigned()->default(0);    
+            $table->float('sell_price')->nullable();
+            $table->float('purchase_price')->nullable();
+            $table->float('box_price')->nullable();
+            $table->integer('amount')->unsigned()->default(0)->nullable();    
+            $table->integer('shrink_pieces')->unsigned()->default(0)->nullable();    
+            $table->integer('box_pieces')->unsigned()->default(0)->nullable();    
             $table->unsignedBigInteger('category_id');
             $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
             $table->timestamps();
